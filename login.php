aebff7c95b80261961eaf3d5bd90b59c4ac64f6e
bobgratton123 <86206987+bobgratton123@users.noreply.github.com>
2021-06-24 16:25:44 -0400

    test loginphp

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+    $personnages = [
+        "Gandalf" => ["Force" => 1, "Dextérité" => 2, "Intelligence" => 6, "Sagesse" => 6, "Charisme" => 5],
+        "Conan" => ["Force" => 5, "Dextérité" => 4, "Intelligence" => 3, "Sagesse" => 3, "Charisme" => 6],
+        //"Barbare" => ["Force" => 6, "Dextérité" => 3, "Intelligence" => 6, "Sagesse" => 4, "Charisme" => 4],
+        "Furiosa" => ["Force" => 6, "Dextérité" => 5, "Intelligence" => 5, "Sagesse" => 1, "Charisme" => 5],
+        "GuyomeLeGrand" => ["Force" => 1, "Dextérité" => 3, "Intelligence" => 4, "Sagesse" => 5, "Charisme" => 3]
+    ];
+
+
+    ?>
